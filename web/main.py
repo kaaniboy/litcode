293bd4ddb5aa080a1a5e2c2d08fc34ebaf701769
Kaan Aksoy <kaanaksoyaz@gmail.com>
2019-01-15 13:47:58 -0700

    Pass port to socketio

--- web/main.py
+++ web/main.py
@@ -163,8 +163,8 @@ def _get_room_info(room):
     }
 
 if __name__ == '__main__':
-    socketio.run(app)
-
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+
+    socketio.run(app, port=port)
     app.run(host='0.0.0.0', port=port, debug=True)
