05776566095a1314616a552fb55dd44eeb656da6
steven85048 <steven85048@gmail.com>
2019-01-14 23:33:34 -0700

    Move endpoints to router; split up some main.py code; added in subdirectories and tracked their paths

--- web/main.py
+++ web/main.py
@@ -6,161 +6,19 @@ from bs4 import BeautifulSoup
 import random
 import string
 
+# MUST configure paths before any non-main directory paths are used
+import path_setup
+path_setup.setupPaths()
+
+from app_router import Routes
+
 app = Flask(__name__)
 app.secret_key = 'flex'
 app.config['SESSION_TYPE'] = 'filesystem'
 
 socketio = SocketIO(app)
 
-rooms = {}
-sockets = {}
-
-PROBLEMS = ['1. Two Sum', '15. 3Sum', '18. 4Sum']
-
-@app.route('/', methods=['GET'])
-def index_get():
-    create_room = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
-
-    view_room = ''
-    if 'view_room' in session:
-        view_room = session['view_room']
-    
-    create_tab = True
-
-    if 'tab' in session and session['tab'] == 'view_room':
-        create_tab = False
-    
-    message = ''
-    if 'message' in session:
-        message = session['message']
-    
-    return render_template('index.html', create_room=create_room, view_room=view_room, message=message, create_tab=create_tab)
-
-@app.route('/create_room', methods=['POST'])
-def create_room_post():
-    room = request.form['room']
-    password = request.form['room_password']
-
-    if room in rooms:
-        session['message'] = 'The room already exists.'
-        return redirect('/')
-    
-    session.clear()
-
-    rooms[room] = {
-        'password': password,
-        'problem': '1. Two Sum',
-        'accepted_players': []
-    }
-
-    return redirect('/room/%s' % room)
-
-@app.route('/view_room', methods=['POST'])
-def view_room_post():
-    room = request.form['room']
-    password = request.form['room_password']
-
-    if room in rooms and rooms[room]['password'] == password:
-        session.clear()
-        return redirect('/room/%s' % room)
-    else:
-        session['tab'] = 'view_room'
-        session['view_room'] = room
-        session['message'] = 'Either the room or its password is incorrect.'
-
-        return redirect('/')
-
-@app.route('/room/<room>', methods=['GET'])
-def room_get(room):
-    return render_template('room.html', room=room)
-
-@socketio.on('join')
-def handle_join(data):
-    name = data['name']
-    room = data['room']
-    
-    join_room(room)
-
-    sockets[request.sid] = {
-        'name': name,
-        'room': room,
-        'code': ''
-    }
-
-    emit('room_info', _get_room_info(room), room=room)
-    emit('join_accepted')
-
-    print(str(sockets))
-
-@socketio.on('disconnect')
-def handle_leave():
-    room = sockets[request.sid]['room']
-    del sockets[request.sid]
-
-    emit('room_info', _get_room_info(room), room=room)
-
-    print(str(sockets))
-
-@socketio.on('run')
-def handle_run(data):
-    name = sockets[request.sid]['name']
-    room = sockets[request.sid]['room']
-
-    if data['problem'] != rooms[room]['problem']:
-        return
-
-    emit('run', {'name': name, 'room': room}, room=room)
-    
-    print('%s ran code in room %s' % (name, room))
-
-@socketio.on('solution_accepted')
-def handle_solution_accepted(data):
-    name = sockets[request.sid]['name']
-    room = sockets[request.sid]['room']
-
-    if data['problem'] != rooms[room]['problem']:
-        return
-    
-    sockets[request.sid]['code'] = data['code']
-
-    if sockets[request.sid] not in rooms[room]['accepted_players']:
-        rooms[room]['accepted_players'].append(sockets[request.sid])
-
-    emit('solution_accepted', {'name': name, 'room': room}, room=room)
-    emit('room_info', _get_room_info(room), room=room)
-
-@socketio.on('solution_declined')
-def handle_solution_declined(data):
-    name = sockets[request.sid]['name']
-    room = sockets[request.sid]['room']
-
-    if data['problem'] != rooms[room]['problem']:
-        return
-
-    emit('solution_declined', {'name': name, 'room': room}, room=room)
-    emit('room_info', _get_room_info(room), room=room)
-
-@socketio.on('next_problem')
-def handle_next_problem(data):
-    room = sockets[request.sid]['room']
-
-    rooms[room]['problem'] = random.choice(PROBLEMS)
-    rooms[room]['accepted_players'] = []
-
-    emit('room_info', _get_room_info(room), room=room)
-
-def _get_players_in_room(room):
-    return [s for s in sockets.values() if s['room'] == room and s['name'] != None]
-
-def _get_room_info(room):
-    players = _get_players_in_room(room)
-    accepted_players = rooms[room]['accepted_players']
-
-    return {
-        'problem': rooms[room]['problem'],
-        'players': players,
-        'accepted_players': accepted_players
-    }
+appRoutes = Routes( socketio, app )
 
 if __name__ == '__main__':
     socketio.run(app)
---
+++ web/path_setup.py
@@ -0,0 +1,6 @@
+import sys
+
+def setupPaths():
+    sys.path.append( './routes' )
+    sys.path.append( './routes/user_routes' )
+    sys.path.append( './routes/admin_routes' )
